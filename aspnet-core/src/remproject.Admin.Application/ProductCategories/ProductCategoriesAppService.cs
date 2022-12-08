8fe49c2a4b662449510cf841db56cc2f233cb611
rem <nthuyen6797@gmail.com>
2022-12-09 06:18:55 +0700

    2.6 Setup CRUD API for Product categories

---
+++ aspnet-core/src/remproject.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,22 @@
+﻿using remproject.ProductCategories;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace remproject.Admin.ProductCategories
+{
+    public class ProductCategoriesAppService : CrudAppService<
+        ProductCategory,
+        ProductCategoryDto,
+        Guid,
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto,
+        CreateUpdateProductCategoryDto>, IProductCategoriesAppService
+    {
+        public ProductCategoriesAppService(IRepository<ProductCategory, Guid> repository)
+            : base(repository)
+        {
+        }
+    }
+}
