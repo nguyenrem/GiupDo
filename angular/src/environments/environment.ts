64f9262e41dd941efe9fcd1affbce7b05a06b437
rem <nthuyen6797@gmail.com>
2022-12-08 20:13:54 +0700

    2.4 Complete login form and get token from Openiddict

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -13,6 +13,7 @@ export const environment = {
     issuer: 'https://localhost:5000/',
     redirectUri: baseUrl,
     clientId: 'remproject_Admin',
+    dummyClientSecret:'1q2w3e*',
     responseType: 'code',
     scope: 'offline_access remproject.Admin',
     requireHttps: true,
