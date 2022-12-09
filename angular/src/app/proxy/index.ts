bdc167340dfb0670ea15a20a3df4966e9cfd867a
rem <nthuyen6797@gmail.com>
2022-12-09 11:49:36 +0700

    2.9 Generate proxy using ABP CLI

---
+++ angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as ProductCategories from './product-categories';
+import * as Products from './products';
+import * as Remproject from './remproject';
+export * from './models';
+export { ProductCategories, Products, Remproject };
