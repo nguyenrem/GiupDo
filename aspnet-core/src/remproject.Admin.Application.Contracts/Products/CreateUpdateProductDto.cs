3b2c07199c8f756541518f9002e72dcf4d970786
rem <nthuyen6797@gmail.com>
2022-12-11 05:24:38 +0700

    2.16 Build add update product form

--- aspnet-core/src/remproject.Admin.Application.Contracts/Products/CreateUpdateProductDto.cs
+++ aspnet-core/src/remproject.Admin.Application.Contracts/Products/CreateUpdateProductDto.cs
@@ -14,6 +14,7 @@ namespace remproject.Admin.Products
         public int SortOrder { get; set; }
         public bool Visiblity { get; set; }
         public bool IsActive { get; set; }
+        public double SellPrice { get; set; }
         public Guid CategoryId { get; set; }
         public string SeoMetaDescription { get; set; }
         public string Description { get; set; }
