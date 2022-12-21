6d4f9b2f9d1efd2628559c8da69b50474738f65b
rem <nthuyen6797@gmail.com>
2022-12-21 22:41:23 +0700

    2.36 Manage user frontend

--- angular/src/app/proxy/index.ts
+++ angular/src/app/proxy/index.ts
@@ -1,9 +1,7 @@
-import * as Manufacturers from './manufacturers';
-import * as ProductAttributes from './product-attributes';
-import * as ProductCategories from './product-categories';
-import * as Products from './products';
+import * as Catalog from './catalog';
 import * as Remproject from './remproject';
-import * as Roles from './roles';
+import * as System from './system';
+import * as Users from './users';
 import * as Volo from './volo';
 export * from './models';
-export { Manufacturers, ProductAttributes, ProductCategories, Products, Remproject, Roles, Volo };
+export { Catalog, Remproject, System, Users, Volo };
